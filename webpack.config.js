d5856095909f0d821739e27586fcb016db29c90a
Michael Bull <mikebull94@gmail.com>
2016-09-23 21:20:18 +0100

    Correct indentation in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,75 +2,75 @@ var webpack = require('webpack'),
     AureliaWebPackPlugin = require('aurelia-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: [
-      './src/main.ts'
-    ],
-    aurelia: [
-      'aurelia-bootstrapper-webpack',
-      'aurelia-binding',
-      'aurelia-dependency-injection',
-      'aurelia-event-aggregator',
-      'aurelia-framework',
-      'aurelia-history',
-      'aurelia-history-browser',
-      'aurelia-loader',
-      'aurelia-loader-webpack',
-      'aurelia-logging',
-      'aurelia-logging-console',
-      'aurelia-metadata',
-      'aurelia-pal',
-      'aurelia-pal-browser',
-      'aurelia-path',
-      'aurelia-polyfills',
-      'aurelia-route-recognizer',
-      'aurelia-router',
-      'aurelia-task-queue',
-      'aurelia-templating',
-      'aurelia-templating-binding',
-      'aurelia-templating-router',
-      'aurelia-templating-resources'
-    ]
-  },
-
-  output: {
-    path: './dist',
-    publicPath: 'dist/',
-    filename: '[name].js'
-  },
-
-  resolve: {
-      extensions: [
-          '',
-          '.webpack.js',
-          '.web.js',
-          '.js',
-          '.jsx',
-          '.ts',
-          '.tsx'
-      ]
-  },
-
-  module: {
-    loaders: [
-      {
-        test: /\.ts$/,
-        loader: 'awesome-typescript-loader'
-      },
-      {
-        test: /\.html$/,
-        loader: 'html'
-      }
-    ]
-  },
-
-  plugins: [
-      new AureliaWebPackPlugin(),
-
-      new webpack.optimize.CommonsChunkPlugin({
-        name: [
-          'aurelia'
+    entry: {
+        app: [
+            './src/main.ts'
+        ],
+        aurelia: [
+            'aurelia-bootstrapper-webpack',
+            'aurelia-binding',
+            'aurelia-dependency-injection',
+            'aurelia-event-aggregator',
+            'aurelia-framework',
+            'aurelia-history',
+            'aurelia-history-browser',
+            'aurelia-loader',
+            'aurelia-loader-webpack',
+            'aurelia-logging',
+            'aurelia-logging-console',
+            'aurelia-metadata',
+            'aurelia-pal',
+            'aurelia-pal-browser',
+            'aurelia-path',
+            'aurelia-polyfills',
+            'aurelia-route-recognizer',
+            'aurelia-router',
+            'aurelia-task-queue',
+            'aurelia-templating',
+            'aurelia-templating-binding',
+            'aurelia-templating-router',
+            'aurelia-templating-resources'
         ]
-      }),
-  ],
+    },
+
+    output: {
+      path: './dist',
+      publicPath: 'dist/',
+      filename: '[name].js'
+    },
+
+    resolve: {
+        extensions: [
+            '',
+            '.webpack.js',
+            '.web.js',
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx'
+        ]
+    },
+
+    module: {
+      loaders: [
+        {
+          test: /\.ts$/,
+          loader: 'awesome-typescript-loader'
+        },
+        {
+          test: /\.html$/,
+          loader: 'html'
+        }
+      ]
+    },
+
+    plugins: [
+        new AureliaWebPackPlugin(),
+
+        new webpack.optimize.CommonsChunkPlugin({
+            name: [
+                'aurelia'
+            ]
+        }),
+    ],
 }
