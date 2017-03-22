748f84b71d90d61e07e035f6ccb8e62e38dc2cea
Michael Bull <mikebull94@gmail.com>
2017-03-22 11:16:19 +0000

    Update dependencies

--- test/e2e/home.e2e.ts
+++ test/e2e/home.e2e.ts
@@ -7,10 +7,10 @@ describe('home page', () => {
     });
 
     it('should set the page title', () => {
-        expect(browser.getTitle()).toBe('Home | My Aurelia App');
+        expect<any>(browser.getTitle()).toBe('Home | My Aurelia App');
     });
 
     it('should set the page heading', () => {
-        expect(heading()).toBe('Home page');
+        expect<any>(heading()).toBe('Home page');
     });
 });
